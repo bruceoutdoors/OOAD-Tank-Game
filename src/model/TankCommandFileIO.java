ed4f6b0048b1dcf17da261b3995d09fbe7242b28
Lee Zhen Yong <bruceoutdoors@gmail.com>
2016-08-27 18:02:48 +0800

    read write file

---
+++ src/model/TankCommandFileIO.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+/**
+ *
+ * @author bruceoutdoors
+ */
+public class TankCommandFileIO {
+
+    private Tank m_tank;
+
+    public TankCommandFileIO(Tank t) {
+        m_tank = t;
+    }
+
+    public TankCommandStack read(String path) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        String filecontents = new String(encoded, Charset.defaultCharset());
+        filecontents = filecontents.trim();
+        String commandStrs[] = filecontents.split(" ");
+        TankCommandStack tcs = new TankCommandStack(commandStrs.length);
+
+        for (String c : commandStrs) {
+            if ("AT".equals(c)) {
+                tcs.addAndExecute(new AttackCommand(m_tank, Tile.Direction.TOP));
+            } else if ("AB".equals(c)) {
+                tcs.addAndExecute(new AttackCommand(m_tank, Tile.Direction.BOTTOM));
+            } else if ("AR".equals(c)) {
+                tcs.addAndExecute(new AttackCommand(m_tank, Tile.Direction.RIGHT));
+            } else if ("AL".equals(c)) {
+                tcs.addAndExecute(new AttackCommand(m_tank, Tile.Direction.LEFT));
+            } else if ("MT".equals(c)) {
+                tcs.addAndExecute(new MoveCommand(m_tank, Tile.Direction.TOP));
+            } else if ("MB".equals(c)) {
+                tcs.addAndExecute(new MoveCommand(m_tank, Tile.Direction.BOTTOM));
+            } else if ("MR".equals(c)) {
+                tcs.addAndExecute(new MoveCommand(m_tank, Tile.Direction.RIGHT));
+            } else if ("ML".equals(c)) {
+                tcs.addAndExecute(new MoveCommand(m_tank, Tile.Direction.LEFT));
+            }
+        }
+        return tcs;
+    }
+
+    public void write(TankCommandStack tcs, String path) throws FileNotFoundException {
+        String commandStr = "";
+
+        Iterator<ITankCommand> iter = tcs.getIterator();
+        while (iter.hasNext()) {
+            ITankCommand itc = iter.next();
+
+            if (itc instanceof MoveCommand) {
+                switch (itc.getDirection()) {
+                    case BOTTOM:
+                        commandStr += "MB"; 
+                        break;
+                    case TOP:
+                        commandStr += "MT";
+                        break;
+                    case RIGHT:
+                        commandStr += "MR";
+                        break;
+                    case LEFT:
+                        commandStr += "ML";
+                        break;
+                }
+            } else if (itc instanceof AttackCommand) {
+                switch (itc.getDirection()) {
+                    case BOTTOM:
+                        commandStr += "AB";
+                        break;
+                    case TOP:
+                        commandStr += "AT";
+                        break;
+                    case RIGHT:
+                        commandStr += "AR";
+                        break;
+                    case LEFT:
+                        commandStr += "AL";
+                        break;
+                }
+            } else {
+                throw new RuntimeException("Unhandled tank command!");
+            }
+            commandStr += " ";
+        }
+
+        try (PrintWriter out = new PrintWriter(path)) {
+            out.println(commandStr);
+        }
+    }
+}
--- src/model/TestBoard.java
+++ src/model/TestBoard.java
@@ -5,13 +5,15 @@
  */
 package model;
 
+import java.io.IOException;
+
 /**
  *
  * @author bruceoutdoors
  */
 public class TestBoard {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Board b = new Board(4, 4);
        // for (int i =0; i < b.getRowCount())
         Tile t = b.getBoardArr()[3][3];
@@ -23,6 +25,13 @@ public class TestBoard {
         player.attack(Tile.Direction.TOP);
         mc.undo();
         System.out.println("hello");
+        
+        TankCommandFileIO io = new TankCommandFileIO(b.getEnemyTank());
+        TankCommandStack s = io.read("SAVED_COMMANDS.txt");
+
+        System.out.println(s);
+        
+        io.write(s, "WRITTEN_COMMANDS.txt");
     }
 
 }
